fd118a417fc075c81a3c4fceb9d7b00d0646b208
ronen <narkisr@gmail.com>
2014-11-25 00:25:05 +0200

    latest opsk spec and adding docker

--- static-modules/aptly/manifests/init.pp
+++ static-modules/aptly/manifests/init.pp
@@ -16,4 +16,26 @@ class aptly {
   package{'bzip2':
     ensure  => present
   }
+
+  file { '/etc/aptly.conf':
+    ensure => file,
+    mode   => '0644',
+    source => 'puppet:///modules/aptly/aptly.conf',
+    owner  => root,
+    group  => root,
+  }
+
+  if($::virtual == 'docker') {
+    file{'/etc/service/aptly':
+      ensure => directory,
+    }
+
+    file { '/etc/service/aptly/run':
+      ensure => file,
+      mode   => 'u+x',
+      source => 'puppet:///modules/aptly/aptly_run',
+      owner  => root,
+      group  => root,
+    }
+  }
 }
